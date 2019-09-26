5c1b87e54f5b670c899e8d44440b8310414ed9a3
HongTao <hongtao@iie.ac.cn>
2019-09-26 11:37:20 +0800

    init project

---
+++ README.md
@@ -0,0 +1,5 @@
+# 测试驱动开发——TDD-BowlingGame
+
+### 参考博客
+
+* [如何说服你的同事使用TDD](https://www.jianshu.com/p/9599083f33bc)
---
+++ src/main/java/com/example/bowlinggame/TddBowlingGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.bowlinggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddBowlingGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddBowlingGameApplication.class, args);
+	}
+
+}
